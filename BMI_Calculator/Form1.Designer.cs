214624e69c20f858f45a85ca5a328e061c919cee
Gurmanpreet Kaur <gkaur286@my.centennialcollege.ca>
2017-08-04 17:14:32 -0400

    Made the property of multiline text box as enabled false

--- BMI_Calculator/Form1.Designer.cs
+++ BMI_Calculator/Form1.Designer.cs
@@ -180,6 +180,7 @@
             // 
             // textBox2
             // 
+            this.textBox2.Enabled = false;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.Location = new System.Drawing.Point(146, 219);
             this.textBox2.Multiline = true;
